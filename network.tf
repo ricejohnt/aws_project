52e57ab81043cd5b7e70bcb5c6da96646826bac7
ricejohnt <61527953+ricejohnt@users.noreply.github.com>
2020-02-28 19:40:21 -0600

    Add files via upload

--- network.tf
+++ network.tf
@@ -1,29 +1,40 @@
-resource "aws_internet_gateway" "default" {
-	vpc_id = "${aws_vpc.default.id}"
+resource "aws_internet_gateway" "igw" {
+	vpc_id = aws_vpc.jr_vpc.id
 }
 
-resource "aws_route_table" "default" {
-	vpc_id = "${aws_vpc.default.id}"
+resource "aws_route_table" "aws_rtb_public" {
+	vpc_id = aws_vpc.jr_vpc.id
 
 	route {
 		cidr_block = "0.0.0.0/0"
-		gateway_id = "${aws_internet_gateway.default.id}"
+		gateway_id = aws_internet_gateway.igw.id
 	}
 }
 
-resource "aws_route_table_association" "table1" {
-	subnet_id = "${aws_subnet.nat_gateway1.id}"
-	route_table_id = "{aws_rtb.default.id}"
+resource "aws_nat_gateway" "nat_gateway1b" {
+	allocation_id = aws_eip.eip_zone_1b.id
+	subnet_id = aws_subnet.sub_nat_gateway1b.id
+	depends_on = [aws_internet_gateway.igw]
 }
 
-resource "aws_route_table_association" "table2" {
-	subnet_id = "${aws_subnet.nat_gateway2.id}"
-	route_table_id = "{aws_rtb.default.id}"
+resource "aws_nat_gateway" "nat_gateway1c" {
+	allocation_id = aws_eip.eip_zone_1c.id
+	subnet_id = aws_subnet.sub_nat_gateway1c.id
+	depends_on = [aws_internet_gateway.igw]
 }
 
+resource "aws_route_table_association" "table1b" {
+	subnet_id = aws_subnet.sub_nat_gateway1b.id
+	route_table_id = aws_route_table.aws_rtb_public.id
+}
+
+resource "aws_route_table_association" "table1c" {
+	subnet_id = aws_subnet.sub_nat_gateway1c.id
+	route_table_id = aws_route_table.aws_rtb_public.id
+}
 
 resource "aws_security_group" "ssh-allowed" {
-	vpc_id = "${aws_vpc.default.id}"
+	vpc_id = aws_vpc.jr_vpc.id
 
 	egress {
 		from_port	= 0
@@ -41,7 +52,7 @@ resource "aws_security_group" "ssh-allowed" {
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 
-	// This is for NGINX
+	// This is for NGINX/HTTP
 	ingress {
 		from_port	= 80
 		to_port	= 80
